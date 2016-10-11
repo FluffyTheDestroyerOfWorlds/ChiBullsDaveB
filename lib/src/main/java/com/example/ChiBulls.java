aa28393098ad60fc7bccc9155807f33faab54816
FluffyTheDestroyerOfWorlds <theboesens@comcast.net>
2016-10-10 22:00:01 -0500

    main

---
+++ lib/src/main/java/com/example/ChiBulls.java
@@ -0,0 +1,119 @@
+package com.example;
+
+public class ChiBulls {
+    public static void main(String[] args) {
+        String[][] BullsRoster = new String[14][22];
+
+        BullsRoster[0][0] = "Dwyane Wade";	BullsRoster[0][1] = "1";	BullsRoster[0][2] = "11.7";	BullsRoster[0][3] = "2";	BullsRoster[0][4] = "3";	BullsRoster[0][5] = "66.7";	BullsRoster[0][6] = "2";	BullsRoster[0][7] = "2";	BullsRoster[0][8] = "100";	BullsRoster[0][9] = "0";	BullsRoster[0][10] = "2";	BullsRoster[0][11] = "0";	BullsRoster[0][12] = "0";	BullsRoster[0][13] = "2";	BullsRoster[0][14] = "2";	BullsRoster[0][15] = "1";	BullsRoster[0][16] = "2";	BullsRoster[0][17] = "2";	BullsRoster[0][18] = "0";	BullsRoster[0][19] = "3";	BullsRoster[0][20] = "6";	BullsRoster[0][21] = "-2";
+        BullsRoster[1][0] = "Jerian Grant";	BullsRoster[1][1] = "1";	BullsRoster[1][2] = "15.1";	BullsRoster[1][3] = "3";	BullsRoster[1][4] = "4";	BullsRoster[1][5] = "75";	BullsRoster[1][6] = "1";	BullsRoster[1][7] = "1";	BullsRoster[1][8] = "100";	BullsRoster[1][9] = "0";	BullsRoster[1][10] = "0";	BullsRoster[1][11] = "0";	BullsRoster[1][12] = "0";	BullsRoster[1][13] = "0";	BullsRoster[1][14] = "0";	BullsRoster[1][15] = "2";	BullsRoster[1][16] = "2";	BullsRoster[1][17] = "0";	BullsRoster[1][18] = "0";	BullsRoster[1][19] = "2";	BullsRoster[1][20] = "7";	BullsRoster[1][21] = "-15";
+        BullsRoster[2][0] = "Jimmy Butler";	BullsRoster[2][1] = "1";	BullsRoster[2][2] = "15.1";	BullsRoster[2][3] = "2";	BullsRoster[2][4] = "5";	BullsRoster[2][5] = "40";	BullsRoster[2][6] = "1";	BullsRoster[2][7] = "1";	BullsRoster[2][8] = "100";	BullsRoster[2][9] = "8";	BullsRoster[2][10] = "9";	BullsRoster[2][11] = "88.9";	BullsRoster[2][12] = "1";	BullsRoster[2][13] = "2";	BullsRoster[2][14] = "3";	BullsRoster[2][15] = "1";	BullsRoster[2][16] = "0";	BullsRoster[2][17] = "1";	BullsRoster[2][18] = "1";	BullsRoster[2][19] = "2";	BullsRoster[2][20] = "13";	BullsRoster[2][21] = "1";
+        BullsRoster[3][0] = "Bobby Portis";	BullsRoster[3][1] = "1";	BullsRoster[3][2] = "26.2";	BullsRoster[3][3] = "2";	BullsRoster[3][4] = "8";	BullsRoster[3][5] = "25";	BullsRoster[3][6] = "1";	BullsRoster[3][7] = "3";	BullsRoster[3][8] = "33.3";	BullsRoster[3][9] = "1";	BullsRoster[3][10] = "2";	BullsRoster[3][11] = "50";	BullsRoster[3][12] = "0";	BullsRoster[3][13] = "9";	BullsRoster[3][14] = "9";	BullsRoster[3][15] = "3";	BullsRoster[3][16] = "1";	BullsRoster[3][17] = "1";	BullsRoster[3][18] = "0";	BullsRoster[3][19] = "2";	BullsRoster[3][20] = "6";	BullsRoster[3][21] = "-8";
+        BullsRoster[4][0] = "Cristiano Felicio";	BullsRoster[4][1] = "1";	BullsRoster[4][2] = "9.2";	BullsRoster[4][3] = "2";	BullsRoster[4][4] = "2";	BullsRoster[4][5] = "100";	BullsRoster[4][6] = "0";	BullsRoster[4][7] = "0";	BullsRoster[4][8] = "0";	BullsRoster[4][9] = "0";	BullsRoster[4][10] = "2";	BullsRoster[4][11] = "0";	BullsRoster[4][12] = "0";	BullsRoster[4][13] = "0";	BullsRoster[4][14] = "0";	BullsRoster[4][15] = "0";	BullsRoster[4][16] = "1";	BullsRoster[4][17] = "1";	BullsRoster[4][18] = "0";	BullsRoster[4][19] = "2";	BullsRoster[4][20] = "4";	BullsRoster[4][21] = "11";
+        BullsRoster[5][0] = "Denzel Valentine";	BullsRoster[5][1] = "1";	BullsRoster[5][2] = "11.1";	BullsRoster[5][3] = "2";	BullsRoster[5][4] = "5";	BullsRoster[5][5] = "40";	BullsRoster[5][6] = "0";	BullsRoster[5][7] = "2";	BullsRoster[5][8] = "0";	BullsRoster[5][9] = "0";	BullsRoster[5][10] = "0";	BullsRoster[5][11] = "0";	BullsRoster[5][12] = "0";	BullsRoster[5][13] = "1";	BullsRoster[5][14] = "1";	BullsRoster[5][15] = "2";	BullsRoster[5][16] = "1";	BullsRoster[5][17] = "0";	BullsRoster[5][18] = "0";	BullsRoster[5][19] = "1";	BullsRoster[5][20] = "4";	BullsRoster[5][21] = "-10";
+        BullsRoster[6][0] = "Doug McDermott";	BullsRoster[6][1] = "1";	BullsRoster[6][2] = "25.7";	BullsRoster[6][3] = "5";	BullsRoster[6][4] = "10";	BullsRoster[6][5] = "50";	BullsRoster[6][6] = "0";	BullsRoster[6][7] = "3";	BullsRoster[6][8] = "0";	BullsRoster[6][9] = "3";	BullsRoster[6][10] = "4";	BullsRoster[6][11] = "75";	BullsRoster[6][12] = "1";	BullsRoster[6][13] = "4";	BullsRoster[6][14] = "5";	BullsRoster[6][15] = "1";	BullsRoster[6][16] = "2";	BullsRoster[6][17] = "1";	BullsRoster[6][18] = "0";	BullsRoster[6][19] = "4";	BullsRoster[6][20] = "13";	BullsRoster[6][21] = "-8";
+        BullsRoster[7][0] = "Isaiah Canaan";	BullsRoster[7][1] = "1";	BullsRoster[7][2] = "15.5";	BullsRoster[7][3] = "1";	BullsRoster[7][4] = "5";	BullsRoster[7][5] = "20";	BullsRoster[7][6] = "0";	BullsRoster[7][7] = "3";	BullsRoster[7][8] = "0";	BullsRoster[7][9] = "0";	BullsRoster[7][10] = "0";	BullsRoster[7][11] = "0";	BullsRoster[7][12] = "0";	BullsRoster[7][13] = "0";	BullsRoster[7][14] = "0";	BullsRoster[7][15] = "1";	BullsRoster[7][16] = "0";	BullsRoster[7][17] = "0";	BullsRoster[7][18] = "0";	BullsRoster[7][19] = "1";	BullsRoster[7][20] = "2";	BullsRoster[7][21] = "4";
+        BullsRoster[8][0] = "Nikola Mirotic";	BullsRoster[8][1] = "1";	BullsRoster[8][2] = "18.8";	BullsRoster[8][3] = "1";	BullsRoster[8][4] = "7";	BullsRoster[8][5] = "14.3";	BullsRoster[8][6] = "0";	BullsRoster[8][7] = "3";	BullsRoster[8][8] = "0";	BullsRoster[8][9] = "0";	BullsRoster[8][10] = "0";	BullsRoster[8][11] = "0";	BullsRoster[8][12] = "1";	BullsRoster[8][13] = "2";	BullsRoster[8][14] = "3";	BullsRoster[8][15] = "1";	BullsRoster[8][16] = "2";	BullsRoster[8][17] = "1";	BullsRoster[8][18] = "1";	BullsRoster[8][19] = "1";	BullsRoster[8][20] = "2";	BullsRoster[8][21] = "-17";
+        BullsRoster[9][0] = "Rajon Rondo";	BullsRoster[9][1] = "1";	BullsRoster[9][2] = "23.7";	BullsRoster[9][3] = "1";	BullsRoster[9][4] = "2";	BullsRoster[9][5] = "50";	BullsRoster[9][6] = "0";	BullsRoster[9][7] = "0";	BullsRoster[9][8] = "0";	BullsRoster[9][9] = "0";	BullsRoster[9][10] = "0";	BullsRoster[9][11] = "0";	BullsRoster[9][12] = "1";	BullsRoster[9][13] = "5";	BullsRoster[9][14] = "6";	BullsRoster[9][15] = "7";	BullsRoster[9][16] = "5";	BullsRoster[9][17] = "0";	BullsRoster[9][18] = "0";	BullsRoster[9][19] = "2";	BullsRoster[9][20] = "2";	BullsRoster[9][21] = "2";
+        BullsRoster[10][0] = "Robin Lopez";	BullsRoster[10][1] = "1";	BullsRoster[10][2] = "19.9";	BullsRoster[10][3] = "4";	BullsRoster[10][4] = "8";	BullsRoster[10][5] = "50";	BullsRoster[10][6] = "0";	BullsRoster[10][7] = "0";	BullsRoster[10][8] = "0";	BullsRoster[10][9] = "0";	BullsRoster[10][10] = "0";	BullsRoster[10][11] = "0";	BullsRoster[10][12] = "1";	BullsRoster[10][13] = "2";	BullsRoster[10][14] = "3";	BullsRoster[10][15] = "1";	BullsRoster[10][16] = "0";	BullsRoster[10][17] = "1";	BullsRoster[10][18] = "3";	BullsRoster[10][19] = "3";	BullsRoster[10][20] = "8";	BullsRoster[10][21] = "4";
+        BullsRoster[11][0] = "Spencer Dinwiddie";	BullsRoster[11][1] = "1";	BullsRoster[11][2] = "9.2";	BullsRoster[11][3] = "1";	BullsRoster[11][4] = "1";	BullsRoster[11][5] = "100";	BullsRoster[11][6] = "0";	BullsRoster[11][7] = "0";	BullsRoster[11][8] = "0";	BullsRoster[11][9] = "1";	BullsRoster[11][10] = "1";	BullsRoster[11][11] = "100";	BullsRoster[11][12] = "0";	BullsRoster[11][13] = "0";	BullsRoster[11][14] = "0";	BullsRoster[11][15] = "1";	BullsRoster[11][16] = "2";	BullsRoster[11][17] = "1";	BullsRoster[11][18] = "0";	BullsRoster[11][19] = "0";	BullsRoster[11][20] = "3";	BullsRoster[11][21] = "11";
+        BullsRoster[12][0] = "Taj Gibson";	BullsRoster[12][1] = "1";	BullsRoster[12][2] = "21.8";	BullsRoster[12][3] = "5";	BullsRoster[12][4] = "9";	BullsRoster[12][5] = "55.6";	BullsRoster[12][6] = "0";	BullsRoster[12][7] = "0";	BullsRoster[12][8] = "0";	BullsRoster[12][9] = "1";	BullsRoster[12][10] = "3";	BullsRoster[12][11] = "33.3";	BullsRoster[12][12] = "5";	BullsRoster[12][13] = "7";	BullsRoster[12][14] = "12";	BullsRoster[12][15] = "1";	BullsRoster[12][16] = "4";	BullsRoster[12][17] = "0";	BullsRoster[12][18] = "1";	BullsRoster[12][19] = "2";	BullsRoster[12][20] = "11";	BullsRoster[12][21] = "6";
+        BullsRoster[13][0] = "Tony Snell";	BullsRoster[13][1] = "1";	BullsRoster[13][2] = "17";	BullsRoster[13][3] = "4";	BullsRoster[13][4] = "7";	BullsRoster[13][5] = "57.1";	BullsRoster[13][6] = "0";	BullsRoster[13][7] = "1";	BullsRoster[13][8] = "0";	BullsRoster[13][9] = "2";	BullsRoster[13][10] = "2";	BullsRoster[13][11] = "100";	BullsRoster[13][12] = "1";	BullsRoster[13][13] = "3";	BullsRoster[13][14] = "4";	BullsRoster[13][15] = "0";	BullsRoster[13][16] = "0";	BullsRoster[13][17] = "0";	BullsRoster[13][18] = "1";	BullsRoster[13][19] = "1";	BullsRoster[13][20] = "10";	BullsRoster[13][21] = "11";
+
+        Double AvgPoints = 0.0;
+// Number 1 Avg Points
+        for (int i = 0; i <13 ; i++) {
+           // System.out.println("A: " + AvgPoints + "------" + Double.parseDouble(BullsRoster[20][i]));
+            AvgPoints += Double.parseDouble(BullsRoster[i][20]);
+        }
+        AvgPoints = AvgPoints / BullsRoster.length;
+        System.out.println("Avg Points are: " + AvgPoints);
+
+// Number 2    average amount of rebounds for the top 3 percentage 3 point shooters
+Double AvgRebound = 0.0;
+        
+        // bleh BubbleSort
+        for (int i = 0; i < 14; i++) {
+            for (int j = 0; j < 14; j++) {
+
+                if(Double.parseDouble(BullsRoster[i][8]) > Double.parseDouble(BullsRoster[j][8])) {
+
+                    String temp[] = BullsRoster[i];
+
+                    BullsRoster[i] = BullsRoster[j];
+                    BullsRoster[j] = temp;
+
+
+                }
+            }
+
+        }
+
+        for (int i = 0; i < 3; i++) {
+            AvgRebound += Double.parseDouble(BullsRoster[i][14]);
+        }
+
+        AvgRebound = AvgRebound / 3;
+        System.out.println("Avg rebounds for the top 3 - 3 pointer shooters is: " + AvgRebound);
+
+
+// Based on the stats who is a better player Dwyane Wade or Jimmy Butler? This season
+int xJimmy = 0;
+int xDwyane  = 0;
+int x= 0;
+
+        x = 14;
+        if (Double.parseDouble(BullsRoster[1][x]) > Double.parseDouble(BullsRoster[2][x])){
+            xJimmy++;
+        } else {
+            xDwyane++;
+        }
+        x = 15;
+        if (Double.parseDouble(BullsRoster[1][x]) > Double.parseDouble(BullsRoster[2][x])){
+            xJimmy++;
+        } else {
+            xDwyane++;
+        }
+        x = 16;
+        if (Double.parseDouble(BullsRoster[1][x]) < Double.parseDouble(BullsRoster[2][x])){
+            xJimmy++;
+        } else {
+            xDwyane++;
+        }
+        x = 17;
+        if (Double.parseDouble(BullsRoster[1][x]) > Double.parseDouble(BullsRoster[2][x])){
+            xJimmy++;
+        } else {
+            xDwyane++;
+        }
+        x = 20;
+
+            if (Double.parseDouble(BullsRoster[1][x]) > Double.parseDouble(BullsRoster[2][x])){
+                xJimmy++;
+            } else {
+                xDwyane++;
+            }
+
+       if( xJimmy > xDwyane) {
+           System.out.println("Jimmy Buttler is the better player.");
+       } else {
+           System.out.println("Dwyane Wade is the better player.");
+       }
+
+
+  /*      for (int i = 0; i < 14; i++) {
+            for (int j = 0; j < 22; j++) {
+                System.out.print(BullsRoster[i][j] + " - ");
+            }
+            System.out.println();
+        }
+
+*/
+    }
+
+
+
+}
+
+
+
+
